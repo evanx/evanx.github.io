97d7e440616903b6977be42e7fe4adf82af8f194
Evan Summers <evan.summers@gmail.com>
2016-05-08 20:41:20 +0200

    ga

--- images/rquery/scripts/crop.sh
+++ images/rquery/scripts/crop.sh
@@ -1,20 +1,57 @@
 
 set -u -e
 
-name=$1
+name=show-ga
 
-crop='600x300+0+0'
+cd $name
+pwd
 
-  cd $name
-  for n in 1 2 3 4 
+mkdir -p cropped
+mkdir -p resized
+mkdir -p geom
+mkdir -p tmp
+
+crop='800x380'
+cropOffset='+5+3'
+shave='65x50'
+resize='1024x512'
+
+convert -size $resize xc:white tmp/canvas.png
+
+c1screenshot() {
+  gnome-screenshot -d 4 -f show-ga/orig/$1.png
+}
+
+c2geom() {
+  convert tmp/canvas.png orig/$1.png -geometry $2 -flatten geom/$1.png 
+}
+
+  rm -f geom/*
+  cp orig/[1-3].png geom/.
+  c2geom 4 '+100+0'
+
+  for n in 1 2 3
   do 
+    orig=orig/$n.png
+    geom=geom/$n.png
     cropped=cropped/$n.png
-    convert -crop $crop orig/$n.png $cropped
+    convert +repage -shave $shave -crop $crop$cropOffset $geom $cropped
     identify -format "\n%wx%h %f" $cropped
   done
-  cd ..
-  convert -delay 200 -loop 0 $name/cropped/[1-4].png $name-animated.gif
+  cp geom/4.png cropped/.
+  for n in 1 2 3 4
+  do 
+    cropped=cropped/$n.png
+    resized=resized/$n.png
+    convert +repage -resize $resize $cropped $resized
+    identify -format "\n%wx%h %f" $resized
+  done
+
+  identify -format "\n%wx%h %f" resized/[1-4].png
+  convert +repage -delay 500 -loop 0 resized/[2-4].png $name-animated.gif
+  identify -format "\n%wx%h %f" $name-animated.gif
+
   ls -l $name-animated.gif
   cp $name-animated.gif ~/Downloads
-  xdg-open $name-animated.gif
+  google-chrome $name-animated.gif
 
