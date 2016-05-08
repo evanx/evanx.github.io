f24c7cb2ca4d6232573d02917f38d5d080cc2905
Evan Summers <evan.summers@gmail.com>
2016-05-08 18:30:42 +0200

    show-ga-animated

--- images/rquery/scripts/crop.sh
+++ images/rquery/scripts/crop.sh
@@ -15,5 +15,6 @@ crop='600x300+0+0'
   cd ..
   convert -delay 200 -loop 0 $name/cropped/[1-4].png $name-animated.gif
   ls -l $name-animated.gif
+  cp $name-animated.gif ~/Downloads
   xdg-open $name-animated.gif
 
